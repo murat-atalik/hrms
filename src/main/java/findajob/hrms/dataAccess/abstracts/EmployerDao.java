45c820bf13946440f37f05fd5f391814c4f47bc2
Murat ATALIK <62357472+murat-atalik@users.noreply.github.com>
2021-05-25 23:56:45 +0300

    exists method added

--- src/main/java/findajob/hrms/dataAccess/abstracts/EmployerDao.java
+++ src/main/java/findajob/hrms/dataAccess/abstracts/EmployerDao.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import findajob.hrms.entities.concretes.Employer;
 
 public interface EmployerDao extends JpaRepository<Employer, Integer> {
-
+	boolean existsEmployerByEmail(String email);
 }
--- src/main/java/findajob/hrms/dataAccess/abstracts/JobSeekerDao.java
+++ src/main/java/findajob/hrms/dataAccess/abstracts/JobSeekerDao.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import findajob.hrms.entities.concretes.JobSeeker;
 
 public interface JobSeekerDao extends JpaRepository<JobSeeker, Integer>{
-
+	boolean existsJobSeekerByNationalityId (String nationalityId);
+	boolean existsJobSeekerByEmail (String email);
 }
